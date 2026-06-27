9969edefc5ad7d48dd62c3831badb7e0622281a1
oritong <oritong@outlook.com>
2026-06-28 02:02:01 +0800

    6.28 upgrate2

--- kubejs/client_scripts/src/waht.js
+++ kubejs/client_scripts/src/waht.js
@@ -1,15 +1,15 @@
-// ClientEvents.paintScreen(e => {
-//     e.player.paint({
-//     1: {
-//         type: 'text',
-//         x: 0,
-//         y: -80,
-//         alignX: 'center',
-//         alignY: 'center',
-//         color: 'white',
-//         text:  'All The Tech正在开发中         测试画面并不代表最终品质           最终解释权归哦里冻所有',
-//         visible: true,
-//         size: 60
-//     }}
-//     )
-// })
+ClientEvents.paintScreen(e => {
+    e.player.paint({
+    1: {
+        type: 'text',
+        x: 0,
+        y: -80,
+        alignX: 'center',
+        alignY: 'center',
+        color: 'white',
+        text:  '钻石大陆嘉豪版正在开发中         测试画面并不代表最终品质           最终解释权归哦里冻所有',
+        visible: true,
+        size: 60
+    }}
+    )
+})
